4bd0378ffcbc355279eac75cd0ccf12e76cb2700
cavc94 <chrystian_tezca@hotmail.com>
2016-08-20 15:22:15 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -1,12 +1,12 @@
-var escena = new TREE.Scene();
-var camara = new TREE.PerspectiveCamera();
+var escena = new THREE.Scene();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
-var renderizador = new TREE.WebGLRenderer();
+var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement );
-var forma = new TREE.BoxGeometry ( 1, 1, 1 );
-var material = new TREE.MeshNormalMaterial();
-var cubo = new TREE.mesh( forma, material );
+var forma = new THREE.BoxGeometry ( 1, 1, 1 );
+var material = new THREE.MeshNormalMaterial();
+var cubo = new THREE.mesh( forma, material );
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
 escena.add( cubo );
