535079e21f3d1778efeab0014be4e79ec0c5f995
cavc94 <chrystian_tezca@hotmail.com>
2016-08-20 15:36:24 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -9,8 +9,7 @@ var material = new THREE.MeshNormalMaterial();
 var cubo = new THREE.Mesh( forma, material );
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
-
-var forma1 = new THREE.CylinderGeometry( 0.5, 0.5, 5, 32 );
+var forma1 = new THREE.CylinderGeometry( 0.5, 0.5, 2.5, 32 );
 var cilindro = new THREE.Mesh( forma1, material );
 escena.add( cubo, cilindro );
 renderizador.render( escena, camara );
