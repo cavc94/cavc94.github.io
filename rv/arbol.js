fddd259b02a5c1a5ca3ea5fd803bf8334992c98d
cavc94 <chrystian_tezca@hotmail.com>
2016-08-23 14:59:35 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -11,7 +11,6 @@ cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
 var forma1 = new THREE.CylinderGeometry( 0.5, 0.5, 5, 32 );
 var cilindro = new THREE.Mesh( forma1, material );
-
+cilindro.translate ( 0, 1, 5 );
 escena.add( cubo, cilindro );
-cilindro.translate ( 0, 0, 1 );
 renderizador.render( escena, camara );
