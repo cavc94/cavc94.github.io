42114fe433944f7d927ca5a6c2b0649bfcbeb93c
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 14:54:06 -0500

    Update ejemplo-revolucion.js

--- rv/ejemplo-revolucion.js
+++ rv/ejemplo-revolucion.js
@@ -1,6 +1,6 @@
 var puntos = [];
 for (var i = 0; i < 50; i ++){
-    puntos.push( new THREE.Vector2( Math.sin(i*0.2)*15+´50, ( i - 5 )*2 ) );
+    puntos.push( new THREE.Vector2( Math.sin(i*0.2)*15+50, ( i - 5 )*2 ) );
     }
 var forma = new THREE.LatheGeomtry(puntos);
 var material = new THREE.MeshNormalMaterial();
