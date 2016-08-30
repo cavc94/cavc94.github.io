0bb132c082491be9197bb666bf1354cce278da83
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 15:24:56 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,6 +1,6 @@
 var troncoForma = new THREE.CylinderGeometry(0.25, 0.5, 1);
 var baseForma = new THREE.CylinderGeometry(0.5, 1, 0.5);
-troncoForma.translate(0,1,0);
+troncoForma.translate(0,0.5,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
 var baseMalla = new THREE.Mesh(baseForma);
