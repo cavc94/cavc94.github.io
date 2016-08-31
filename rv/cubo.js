824645ea800dbd229e68eef123ef6297cf8b19bd
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 22:46:29 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -11,16 +11,7 @@ forma.vertices.push( new THREE.Vector3( 1, 1, 1 ) );
 
 forma.faces.push( new THREE.Face3( 0, 2, 6) );
 forma.faces.push( new THREE.Face3( 0, 4, 6) );
-forma.faces.push( new THREE.Face3( 0, 1, 3) );
-forma.faces.push( new THREE.Face3( 0, 2, 3 ) );
-forma.faces.push( new THREE.Face3( 0, 4, 5 ) );
-forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
-forma.faces.push( new THREE.Face3( 1, 5, 7 ) );
-forma.faces.push( new THREE.Face3( 1, 3, 7 ) );
-forma.faces.push( new THREE.Face3( 2, 3, 7 ) );
-forma.faces.push( new THREE.Face3( 2, 6, 7 ) );
-forma.faces.push( new THREE.Face3( 4, 5, 7 ) );
-forma.faces.push( new THREE.Face3( 4, 6, 7 ) );
+
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
@@ -38,6 +29,6 @@ escena.add( malla );
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerWidth*.95 );
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
