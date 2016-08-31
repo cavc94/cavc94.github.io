b79bfb733619741f2f6e09ac123ec7b526e16799
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 22:43:54 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -38,6 +38,6 @@ escena.add( malla );
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight );
+renderizador.setSize( window.innerHeight*.95, window.innerWidth*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
