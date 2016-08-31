48f429f140f32ddfe3d7867fde353bb4b75b655e
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 22:51:43 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -13,8 +13,8 @@ forma.vertices.push( new THREE.Vector3( 1, 1, 1 ) );
 //forma.faces.push( new THREE.Face3( 6, 4, 0) );
 //forma.faces.push( new THREE.Face3( 0, 1, 3) );
 //forma.faces.push( new THREE.Face3( 0, 2, 3 ) );
-//forma.faces.push( new THREE.Face3( 0, 4, 5 ) );
-forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
+forma.faces.push( new THREE.Face3( 0, 4, 5 ) );
+//forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
 
 
 forma.computeBoundingSphere();
@@ -23,9 +23,9 @@ forma.computeFaceNormals();
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 
-malla.rotateX(-Math.PI/2);
-malla.rotateY(Math.PI/4);
-malla.rotateZ(-Math.PI/3);
+//malla.rotateX(-Math.PI/2);
+//malla.rotateY(Math.PI/4);
+//malla.rotateZ(-Math.PI/3);
 
 var escena = new THREE.Scene();
 escena.add( malla );
