283b37da87c831b992f6a3dbea8959ebfa28f8f9
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 21:28:59 -0500

    Create cubo.js

---
+++ rv/cubo.js
@@ -0,0 +1,33 @@
+var forma = new THREE.Geometry();
+
+forma.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+forma.vertices.push( new THREE.Vector3( 0, 0, 1 ) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 0, 0 ) );
+forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 1, 0 ) );
+forma.vertices.push( new THREE.Vector3( 1, 1, 1 ) );
+
+forma.faces.push( new THREE.Face3( 1, 3, 7) );
+forma.faces.push( new THREE.Face3( 7, 5, 1) );
+forma.faces.push( new THREE.Face3( 1, 3, 4) );
+forma.faces.push( new THREE.Face3( 4, 2, 1) );
+forma.faces.push( new THREE.Face3( 1, 4, 6) );
+forma.faces.push( new THREE.Face3( 6, 5, 1) );
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
