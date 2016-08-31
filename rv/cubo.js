4eccebb21826e8d1ee448c63825b4f29963ad316
cavc94 <chrystian_tezca@hotmail.com>
2016-08-30 22:34:28 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -12,15 +12,15 @@ forma.vertices.push( new THREE.Vector3( 1, 1, 1 ) );
 forma.faces.push( new THREE.Face3( 0, 2, 6) );
 forma.faces.push( new THREE.Face3( 0, 4, 6) );
 forma.faces.push( new THREE.Face3( 0, 1, 3) );
-forma.faces.push( new THREE.Face3( 3, 2, 0 ) );
-forma.faces.push( new THREE.Face3( 1, 5, 4 ) );
-forma.faces.push( new THREE.Face3( 1, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 0, 2, 3 ) );
+forma.faces.push( new THREE.Face3( 0, 4, 5 ) );
+forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
 forma.faces.push( new THREE.Face3( 1, 5, 7 ) );
-forma.faces.push( new THREE.Face3( 7, 3, 1 ) );
-forma.faces.push( new THREE.Face3( 3, 2, 6 ) );
-forma.faces.push( new THREE.Face3( 6, 7, 3 ) );
-forma.faces.push( new THREE.Face3( 6, 4, 5 ) );
-forma.faces.push( new THREE.Face3( 5, 7, 6 ) );
+forma.faces.push( new THREE.Face3( 1, 3, 7 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 7 ) );
+forma.faces.push( new THREE.Face3( 2, 6, 7 ) );
+forma.faces.push( new THREE.Face3( 4, 5, 7 ) );
+forma.faces.push( new THREE.Face3( 4, 6, 7 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
@@ -34,6 +34,7 @@ malla.rotateZ(-Math.PI/3);
 
 var escena = new THREE.Scene();
 escena.add( malla );
+
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
