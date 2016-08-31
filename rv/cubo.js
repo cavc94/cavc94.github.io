9745a27123d368aed947f6a5a56133ab6870f517
cavc94 <chrystian_tezca@hotmail.com>
2016-08-31 01:00:32 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -12,15 +12,15 @@ forma.vertices.push( new THREE.Vector3( 0, 0, 0 ),
 forma.faces.push( new THREE.Face3( 0, 2, 6),
                   new THREE.Face3( 0, 6, 4),
                   new THREE.Face3( 4, 0, 1),
-                  new THREE.Face3( 1, 3, 2 ),
-                  new THREE.Face3( 2, 1, 5 ),
-                  new THREE.Face3( 5, 4, 1 ),
-                  new THREE.Face3( 5, 7, 6 ),
-                  new THREE.Face3( 5, 6, 3 ),
-                  new THREE.Face3( 7, 3, 5 ) );
-//forma.faces.push( new THREE.Face3( 4, 6, 7 ) );
-//forma.faces.push( new THREE.Face3( 7, 2, 0 ) );
-//forma.faces.push( new THREE.Face3( 4, 6, 7 ) );
+                  new THREE.Face3( 0, 2, 3 ),
+                  new THREE.Face3( 1, 3, 0 ),
+                  new THREE.Face3( 2, 3, 7 ),
+                  new THREE.Face3( 2, 7, 6 ),
+                  new THREE.Face3( 7, 5, 4 ),
+                  new THREE.Face3( 6, 4, 7 ),
+                  new THREE.Face3( 7, 5, 4 ),
+                  new THREE.Face3( 0, 5, 1 ),
+                  new THREE.Face3( 1, 7, 5 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
@@ -28,9 +28,9 @@ forma.computeFaceNormals();
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 
-//malla.rotateX(-Math.PI/2);
-//malla.rotateY(Math.PI/4);
-//malla.rotateZ(-Math.PI/3);
+malla.rotateX(-Math.PI/2);
+malla.rotateY(Math.PI/4);
+malla.rotateZ(-Math.PI/3);
 
 var escena = new THREE.Scene();
 escena.add( malla );
