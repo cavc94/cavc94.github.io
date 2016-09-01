e41933088a739df852f2b7d6f6e4c99a52e86b75
cavc94 <chrystian_tezca@hotmail.com>
2016-08-31 21:11:10 -0500

    Update ejemplo-extrusion.js

--- rv/ejemplo-extrusion.js
+++ rv/ejemplo-extrusion.js
@@ -1,17 +1,23 @@
 var figura = new THREE.Shape();
-figura.moveTo(10, 10);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
-figura.lineTo(10, 10);
-var forma =  new THREE.ExtrudeGeometry( figura, {amount: 10} );
+
+figura.moveTo(10,10);
+
+figura.lineTo(10,40);
+figura.lineTo(40,40);
+figura.lineTo(10,10);
+
+var forma = new THREE.ExtrudeGeometry(figura,{amount:10});
+
 var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/4 );
+var malla = new THREE.Mesh(forma,material);
+malla.rotateX(Math.PI/4);
+
 var escena = new THREE.Scene();
-escena.add( malla );
+escena.add(malla);
+
 var camara = new THREE.PerspectiveCamera();
-camara.position.z= 500;
+camara.position.z=500;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.Height*.95, window.Height*.95);
-document.body.appendChild( renderizador.domElement );
-renderizador.render(escena, camara);
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
