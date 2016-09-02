f502ce091dacc7b8643897d069c06f28ca0aa5f9
cavc94 <chrystian_tezca@hotmail.com>
2016-09-01 21:01:14 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -14,7 +14,7 @@ forma.faces.push( new THREE.Face3( 0, 2, 6 ),
                   new THREE.Face3( 0, 6, 2 ),
                   new THREE.Face3( 0, 1, 3 ),
                   new THREE.Face3( 0, 2, 3 ),
-                  new THREE.Face3( 0, 3, 2 ) );
+                  new THREE.Face3( 0, 3, 1 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
