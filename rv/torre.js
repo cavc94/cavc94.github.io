605692380eba5753ce0011087250beb2a2dfffa6
cavc94 <chrystian_tezca@hotmail.com>
2016-09-01 19:17:47 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -7,26 +7,23 @@ var almenaF2 = new THREE.CylinderGeometry(0.4, 0.4, 0.1, 8, 1, true, Math.PI/2, 
 var almenaF3 = new THREE.CylinderGeometry(0.4, 0.4, 0.1, 8, 1, true, 3*Math.PI/4, Math.PI/4);
 var almenaF4 = new THREE.CylinderGeometry(0.4, 0.4, 0.1, 8, 1, true, Math.PI, Math.PI/4);
 
+troncoForma.translate(0,0.6,0);
+baseForma.translate(0,1.15,0);
+//almenaForma.translate(0,1.25,0.2);
+almenaF1.translate(0,1.25,0);
+almenaF2.translate(0,1.25,0);
+almenaF3.translate(0,1.25,0);
+almenaF4.translate(0,1.25,0);
+
+var troncoMalla = new THREE.Mesh(troncoForma);
+var alamborMalla = new THREE.Mesh(alamborForma);
+//var almenaMalla = new THREE.Mesh(almenaForma);
+var baseMalla = new THREE.Mesh(baseForma);
+
 var almenaM1 = new THREE.Mesh(almenaF1);
 var almenaM2 = new THREE.Mesh(almenaF2);
 var almenaM3 = new THREE.Mesh(almenaF3);
 var almenaM5 = new THREE.Mesh(almenaF4);
-
-var almenaForma = new THREE.Geometry();
-almenaForma.merge(almenaM1.geometry, almenaM1.matrix);
-almenaForma.merge(almenaM2.geometry, almenaM2.matrix);
-almenaForma.merge(almenaM3.geometry, almenaM3.matrix);
-almenaForma.merge(almenaM4.geometry, almenaM4.matrix);
-
-troncoForma.translate(0,0.6,0);
-baseForma.translate(0,1.15,0);
-//almenaForma.translate(0,1.25,0.2);
-almenaForma.translate(0,1.25,0);
-
-var troncoMalla = new THREE.Mesh(troncoForma);
-var alamborMalla = new THREE.Mesh(alamborForma);
-var almenaMalla = new THREE.Mesh(almenaForma);
-var baseMalla = new THREE.Mesh(baseForma);
 
 var torreForma = new THREE.Geometry();
 
@@ -34,6 +31,10 @@ torreForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 torreForma.merge(alamborMalla.geometry, alamborMalla.matrix);
 torreForma.merge(almenaMalla.geometry, almenaMalla.matrix);
 torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+torreForma.merge(almenaM1.geometry, almenaM1.matrix);
+torreForma.merge(almenaM2.geometry, almenaM2.matrix);
+torreForma.merge(almenaM3.geometry, almenaM3.matrix);
+torreForma.merge(almenaM4.geometry, almenaM4.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh(torreForma, material);
