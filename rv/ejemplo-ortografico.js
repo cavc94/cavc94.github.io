af02d9fc384cd0524575d6e04a96a52fa5e0922e
cavc94 <chrystian_tezca@hotmail.com>
2016-09-06 15:43:58 -0500

    Update ejemplo-ortografico.js

--- rv/ejemplo-ortografico.js
+++ rv/ejemplo-ortografico.js
@@ -13,7 +13,7 @@ camara.position.z = 100;
 var cubo = new THREE.Mesh( new THREE.BoxGeometry( 30, 30, 30), new THREE.MeshNormalMaterial() );
 cubo.rotateY( Math.PI/4 );
 
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial() );
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterial() );
 esfera1.position.x = 50;
 
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMaterial() );
