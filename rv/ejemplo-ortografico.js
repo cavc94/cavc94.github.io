cf65abb782a2fd83025118adb0f5bc4557276ebc
cavc94 <chrystian_tezca@hotmail.com>
2016-09-06 15:36:14 -0500

    Update ejemplo-ortografico.js

--- rv/ejemplo-ortografico.js
+++ rv/ejemplo-ortografico.js
@@ -21,9 +21,7 @@ esfera2.position.x = -50;
 esfera2.postion.z = -100;
 
 var escena = new THREE.Scene();
-escena.add( cubo );
-escena.add( esfera1 );
-escena.add( esfera2 );
+escena.add( cubo, esfera1, esfera2 );
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth, window.innerHeight );
