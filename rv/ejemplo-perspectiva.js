629337e691bc3d31f080386262ef68a5a3d1d1af
cavc94 <chrystian_tezca@hotmail.com>
2016-09-06 15:40:53 -0500

    Update ejemplo-perspectiva.js

--- rv/ejemplo-perspectiva.js
+++ rv/ejemplo-perspectiva.js
@@ -4,16 +4,15 @@ var planoCercano = 1;
 var planoLejano = 1000;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
-
 camara.position.z = 15;
 
-var cubo = new THREE.Mesh( new THREE.BoxGeometry(2, 2, 2), new THREE.MeshNormanlMaterial() );
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(2, 2, 2), new THREE.MeshNormalMaterial() );
 cubo.rotateY( Math.PI/4 );
 
-var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 1 ), new THREE.MeshNormalMaterial() );
 esfera1.position.x = 5;
 
-var esfera2 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ), new THREE.MeshNormalMaterial() );
 esfera2.position.x = -5;
 esfera2.position.z = -10;
 
