e4d68ec4dd03f2eeaecf2cd12730424159944d3b
cavc94 <chrystian_tezca@hotmail.com>
2016-09-07 17:14:16 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -6,20 +6,25 @@ var planoLejano = 1000;
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
 camara.position.z = 15;
 
-var casillaN = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), new THREE.MeshNormalMaterial() );
+//for ( var i = 0; i < 3; i ++ ) {
+//	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+//}
+
+var Blanco = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+var Negro = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+
+var casillaN = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), Negro  );
 casillaN.rotateX( -Math.PI/4 );
 
-var casillaB = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), new THREE.MeshNormalMaterial() );
+var casillaB = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), new Blanco );
 casillaB.rotateX( -Math.PI/4 );
 casillaB.translate(0,1,0);
-
-var casillas = [casillaN, casillaB];
 
 //var tablero = new Geometry();
 //tablero.merge(casillaN.geometry, casillaN.matrix);
 
 var escena = new THREE.Scene();
-escena.add( casillas );
+escena.add( casillaN, casillaB );
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth, window.innerHeight );
