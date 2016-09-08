7d84946155ee9ca061c40dd7f1745dc3c47629c4
cavc94 <chrystian_tezca@hotmail.com>
2016-09-08 14:50:48 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,24 +1,16 @@
-var campoVision = 45; //grados
-var relacionAspecto = window.innerWidth / window.innerHeight;
-var planoCercano = 1;
-var planoLejano = 1000;
-
-var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
-camara.position.z = 15;
-
-//for ( var i = 0; i < 3; i ++ ) {
-//	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
-//}
-
 var Blanco = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 var Negro = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 
-var casillaN = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), Negro  );
+for ( var i = 0; i < 3; i ++ ) {
+	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+}
+
+var casillaN = new THREE.Mesh( new THREE.BoxGeometry(10, 5, 10), Negro  );
 casillaN.rotateX( -Math.PI/4 );
 
-var casillaB = new THREE.Mesh( new THREE.BoxGeometry(2, 1, 2), Blanco );
+var casillaB = new THREE.Mesh( new THREE.BoxGeometry(10, 5, 10), Blanco );
 casillaB.rotateX( -Math.PI/4 );
-casillaB.translate(1,0,0);
+casillaB.translate(5,0,0);
 
 //var tablero = new Geometry();
 //tablero.merge(casillaN.geometry, casillaN.matrix);
