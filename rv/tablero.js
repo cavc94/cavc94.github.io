68353efef919df8f5a322375a84c2e41c5eb8d1d
cavc94 <chrystian_tezca@hotmail.com>
2016-09-08 15:26:53 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -3,7 +3,8 @@ var casilla = new THREE.BoxGeometry(10, 5, 10);
 var Blanco = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 var Negro = new THREE.MeshBasicMaterial( { color: 0x555555 } );
 
-var casillas = new Array();
+var casillasN = new Array();
+var casillasB = new Array();
 
 for ( var i = 0; i < 33; i ++ ) {
 	casillasN[i] = new THREE.Mesh( casilla , Negro  );
@@ -35,5 +36,3 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
-
-
