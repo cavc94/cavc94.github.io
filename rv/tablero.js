784cd7a8d203a1b04f366b8ac1430dff6b413f82
cavc94 <chrystian_tezca@hotmail.com>
2016-09-12 16:51:39 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -57,7 +57,7 @@ for ( var i = 0; i < 64; i ++ ) {
 //escena.add( casillasB[0] );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth*.95, window.innerHeight*.95 );
