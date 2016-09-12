8f9d0e781ccb4de3db1e5fb8ffdfd749c64f2876
cavc94 <chrystian_tezca@hotmail.com>
2016-09-12 16:25:35 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -7,7 +7,7 @@ var casillasN = new Array();
 var casillasB = new Array();
 //var casillas = new Array();
 
-for ( var i = 0; i < 32; i ++ ) {
+for ( var i = 0; i < 33; i ++ ) {
 	casillasN[i] = new THREE.Mesh( casilla, Negro );
 	casillasB[i] = new THREE.Mesh( casilla, Blanco );
 }
