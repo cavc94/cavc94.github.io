bb0e860801b0a70c8e89681ec758fd5b356e5b2b
cavc94 <chrystian_tezca@hotmail.com>
2016-09-13 15:33:12 -0500

    Create ejemplo-luzSinSombras.js

---
+++ rv/ejemplo-luzSinSombras.js
@@ -0,0 +1,21 @@
+var iluminacion = new THREE.PointLight( 0xffffff );
+iluminacion.position.y = 20;
+
+var forma = new THREE.SphereGeometry( 1 );
+var material = new THREE.MeshLambertMaterial( {color: "#00cc00"} );
+var malla = new THREE.Mesh( forma, material );
+malla.position.y = 2;
+
+var base = new THREE.Mesh( new THREE.BoxGeometry( 5, 0.1, 5 ), new THREE.MeshLambertMaterial( {color: 0xffffff} ) );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+escena.add(base);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 15;
+camara.position.y = 5;
+
+var lienzo = document.getElementById( "luzSinSombras" );
+var renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
