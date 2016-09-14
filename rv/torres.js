cd9b59b7b8dbc12d2ca1cc03483b0f4c018670f0
cavc94 <chrystian_tezca@hotmail.com>
2016-09-13 21:13:22 -0500

    Update torres.js

--- rv/torres.js
+++ rv/torres.js
@@ -70,7 +70,7 @@ for (var i = 0; i < 4; i ++ ){
 }
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 300;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
