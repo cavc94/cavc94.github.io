8a03724bf7fa96a11cfad1cf728827f49beaf576
cavc94 <chrystian_tezca@hotmail.com>
2016-09-13 20:02:09 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,10 +1,10 @@
-var campoVision = 90; //grados
+var campoVision = 45; //grados
 var relacionAspecto = window.innerWidth / window.innerHeight;
 var planoCercano = 1;
 var planoLejano = 1000;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
-camara.position.z = 300;
+camara.position.set = ( 300, 0,300 );
 
 var casilla = new THREE.BoxGeometry(10, 10, 10);
 
