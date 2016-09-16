57e373a37e1af74b1b4487b8edf8dd92700ed0f5
cavc94 <chrystian_tezca@hotmail.com>
2016-09-16 17:48:21 -0500

    Update tablero_ortografico.js

--- rv/tablero_ortografico.js
+++ rv/tablero_ortografico.js
@@ -1,10 +1,10 @@
 /*Cámara ortográfica*/
 var camara = new THREE.OrthographicCamera();
 
-camara.left = window.innerWidth / -2;
-camara.right = window.innerWidth / 2;
-camara.top = window.innerHeight / 2;
-camara.bottom = window.innerHeight / -2;
+camara.left = window.innerWidth / -10;
+camara.right = window.innerWidth / 10;
+camara.top = window.innerHeight / 10;
+camara.bottom = window.innerHeight / -10;
 camara.near = 0.1;
 camara.far = 20000;
 camara.updateProjectionMatrix();
