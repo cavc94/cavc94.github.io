41e8da672a150b8e877788a3c5cc613c22b676d6
cavc94 <chrystian_tezca@hotmail.com>
2016-09-22 15:39:01 -0500

    Update ejemplo-funciones.js

--- rv/ejemplo-funciones.js
+++ rv/ejemplo-funciones.js
@@ -13,7 +13,7 @@ var loop = function() {
   requestAnimationFrame( loop );
   renderizador.render( escena, camara );
   malla.rotateY( 0.01 );
-  malla.translate.x( 0.01 );
+  camara.position.x( 0.01 );
   }
 
 var escena, camara, renderizador, malla;
