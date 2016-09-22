1a3d930f8e0ba0b3e3039acf0d81198674229567
cavc94 <chrystian_tezca@hotmail.com>
2016-09-22 15:52:19 -0500

    Update ejemplo-funciones.js

--- rv/ejemplo-funciones.js
+++ rv/ejemplo-funciones.js
@@ -14,10 +14,11 @@ var loop = function() {
   requestAnimationFrame( loop );
   renderizador.render( escena, camara );
   malla.rotateY( 0.01 );
-  if (Math.abs( malla.position.x ) >= 2 )
+  if (Math.abs( malla.position.x ) >= 2 && Math.abs( malla.position.y ) >= 2 )
     step = -step;
   malla.position.x+=step;
-  }
+  malla.position.y+=step;
+}
 
 var escena, camara, renderizador, malla, step;
 init(1);
