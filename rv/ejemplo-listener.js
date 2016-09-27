cb3b8869a84e6b6a24e8f0b2b8f81d9720e8a5ad
cavc94 <chrystian_tezca@hotmail.com>
2016-09-27 14:59:01 -0500

    Update ejemplo-listener.js

--- rv/ejemplo-listener.js
+++ rv/ejemplo-listener.js
@@ -12,7 +12,7 @@ VENTANA.setup = function() {
   window.addEventListener( tipo_evento, VENTANA.listener, capturap )
   VENTANA.escena = new THREE.Scene();
   VENTANA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  VENTANA.camnra.position.z = 5;
+  VENTANA.camara.position.z = 5;
   var lienzo = document.getElementById( "ejemplo-ventana" );
   VENTANA.renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
   VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight );
