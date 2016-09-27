f56cd7c630ced3747bdb3a8df1962494a248373b
cavc94 <chrystian_tezca@hotmail.com>
2016-09-27 14:56:08 -0500

    Create ejemplo-listener.js

---
+++ rv/ejemplo-listener.js
@@ -0,0 +1,31 @@
+var VENTANA = new Object();
+
+VENTANA.listener = function() {
+  VENTANA.camara.aspect = window.innerWidth / window.innerHeight;
+  VENTANA.camara.updateProjectionMatrix();
+  VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight ); 
+}
+ 
+VENTANA.setup = function() {
+  var tipo evento = 'resize';
+  var capturap = false;
+  window.addEventListener( tipo_evento, VENTANA.listener, capturap )
+  VENTANA.escena = new THREE.Scene();
+  VENTANA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  VENTANA.camnra.position.z = 5;
+  var lienzo = document.getElementById( "ejemplo-ventana" );
+  VENTANA.renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
+  VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight );
+  VENTANA.malla = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1 ), new THREE.MeshNormalMaterial() );
+  VENTANA.escena.add( VENTANA.malla );
+  }
+  
+ VENTANA.loop = function (){
+  requestAnimationFrame( VENTANA.loop );
+  VENTANA.malla.rotateX( 0.01 );
+  VENTANA.malla.rotateY( 0.01 );
+  VENTANA.renderizador.render( VENTANA.escena, VENTANA.camara );
+  }
+ 
+ VENTANA.setup();
+ VENTANA.loop();
