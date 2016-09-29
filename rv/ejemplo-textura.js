45cac71a7f4b517ebb2c99444f753dec96319731
cavc94 <chrystian_tezca@hotmail.com>
2016-09-29 15:09:51 -0500

    Update ejemplo-textura.js

--- rv/ejemplo-textura.js
+++ rv/ejemplo-textura.js
@@ -8,12 +8,12 @@ TEXTURA.retrollamada = function( textura ){
  }
  
 TEXTURA.setup = function() {
-  TEXTURA.escena = new THREE.Scene();
+  TEXTURA.escena = new THREE.Scene(); 
   
   var cargador = new THREE.TextureLoader();
   cargador.load( "earth_atmos_2048.jpg", TEXTURA.retrollamada );
   
-  TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
   TEXTURA.camara.position.z = 5;
   
   var lienzo = document.getElementById( "ejemplo-textura" );
