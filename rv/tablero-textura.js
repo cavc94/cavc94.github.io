208a169f77d509827cfed1a672c22a8c4a30ef69
cavc94 <chrystian_tezca@hotmail.com>
2016-09-29 16:08:48 -0500

    Update tablero-textura.js

--- rv/tablero-textura.js
+++ rv/tablero-textura.js
@@ -52,8 +52,23 @@ torreForma.scale( 10, 10, 10);
 var TEXTURA = new Object();
 
 TEXTURA.retrollamada = function( textura ){
+  
   var material = new THREE.MeshBasicMaterial( {map: textura} );
-  TEXTURA.malla = new THREE.Mesh( torreForma, material ); 
+  TEXTURA.torreMalla = new Array();
+  for ( var i = 0; i < 4; i ++ ) {  
+    if ( i < 2 ){ 
+      TEXTURA.torreMalla[i] = new THREE.Mesh(torreForma, material);
+      } else {
+        TEXTURA.torreMalla[i] = new THREE.Mesh(torreForma, material); }
+        //torreMalla[i].castShadow = true;
+        TEXTURA.torreMalla[i].rotateX(Math.PI/2);
+      }
+  
+  /*torreMalla[0].position.set( 0, 0, 5 );
+  torreMalla[1].position.set( 70, 0, 5 );
+  torreMalla[2].position.set( 0, 70, 5 );
+  torreMalla[3].position.set( 70, 70, 5 );*/
+  
   TEXTURA.escena.add( TEXTURA.malla );
  }
  
