1ca0efed9788ea5cd2c4e26c5bf1f70e323fcf28
cavc94 <chrystian_tezca@hotmail.com>
2016-10-06 22:01:10 -0500

    Update tablero-textura.js

--- rv/tablero-textura.js
+++ rv/tablero-textura.js
@@ -154,7 +154,7 @@ TEXTURA.setup = function() {
   TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
   TEXTURA.camara.position.z = 120;
   TEXTURA.camara.position.y = -50;
-  TEXTURA.camara.lookAt( new THREE.Vector3(35,35,0) );
+  TEXTURA.camara.lookAt( new THREE.Vector3(35,35,5) );
 
   var lienzo = document.getElementById( "tablero-textura" );
   TEXTURA.renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true } );
