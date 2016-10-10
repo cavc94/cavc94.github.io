aac25b8eb8ffe2cb33ceaa04f0249f28258f7dd7
cavc94 <chrystian_tezca@hotmail.com>
2016-10-10 17:10:57 -0500

    Update peon.js

--- rv/peon.js
+++ rv/peon.js
@@ -6,17 +6,22 @@ PROTOTIPO.Peon = function(){
   var cabezaForma = new THREE.SphereGeometry( 0.3, 32, 32 );
   var troncoForma = new THREE.CylinderGeometry( 0.1, 0.3, 0.5 );
   var adornoForma = new THREE.TorusGeometry(0.3, 0.05, 16, 100);
+  var baseForma = new THREE.CylinderGeometry( 0.3, 0.3, 0.25 );
+  
   cabezaForma.translate( 0, 0.5, 0 );
   adornoForma.rotateX(Math.PI/2);
   adornoForma.translate( 0, -0.25, 0 );
+  baseForma.translate( 0, -0.375, 0 );
   
   var troncoMalla = new THREE.Mesh( troncoForma );
   var cabezaMalla = new THREE.Mesh( cabezaForma );
   var adornoMalla = new THREE.Mesh( adornoForma );
+  var baseMalla = new THREE.Mesh( baseForma );
   
   this.merge( troncoMalla.geometry, troncoMalla.matrix );
   this.merge( cabezaMalla.geometry, cabezaMalla.matrix );
   this.merge( adornoMalla.geometry, adornoMalla.matrix );
+  this.merge( baseMalla.geometry, baseMalla.matrix );
  }
  
 PROTOTIPO.Peon.prototype = new THREE.Geometry();
