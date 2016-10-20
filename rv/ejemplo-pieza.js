81294312f1678fb8e0fd064f157a2daf785ea033
cavc94 <chrystian_tezca@hotmail.com>
2016-10-20 15:21:09 -0500

    Update ejemplo-pieza.js

--- rv/ejemplo-pieza.js
+++ rv/ejemplo-pieza.js
@@ -12,12 +12,12 @@ function Pieza(){
   }
 Pieza.prototype = new THREE.Object3D;
 var pieza = new Pieza();
+var camara = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
 function setup() {
   var escena = new THREE.Scene();
   escena.add( pieza );
-  var camara = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
   var lienzo = document.getElementById( "ejemplo-pieza" );
-  var renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
   renderizador.setSize( 600, 600 );
  }
 function loop(){
