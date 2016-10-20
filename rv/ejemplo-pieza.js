af3d21dc165e9ac0ba263e81b9bac60e2031e176
cavc94 <chrystian_tezca@hotmail.com>
2016-10-20 15:00:11 -0500

    Update ejemplo-pieza.js

--- rv/ejemplo-pieza.js
+++ rv/ejemplo-pieza.js
@@ -1,8 +1,8 @@
 function Pieza(){
   THREE.Object3D.call( this );
-  this.piernaIzq = new THREE.Mesh( new THREE.BoxGeometry( 1, 5, 1 );
-  this.piernaDer = new THREE.Mesh( new THREE.BoxGeometry( 1, 5, 1 );
-  var cuerpo = new THREE.Mesh ( new THREE.BoxGeometry( 5, 10, 5 );
+  this.piernaIzq = new THREE.Mesh( new THREE.BoxGeometry( 1, 5, 1 ) );
+  this.piernaDer = new THREE.Mesh( new THREE.BoxGeometry( 1, 5, 1 ) );
+  var cuerpo = new THREE.Mesh ( new THREE.BoxGeometry( 5, 10, 5 ) );
   this.add( piernaIzq, piernaDer, cuerpo );
   this.piernaIzq.position.z = -2;
   this.piernaIzq.position.y = -2.5;
