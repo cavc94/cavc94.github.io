b783ff6806a774d84a0f04564e689dd752044b6c
cavc94 <chrystian_tezca@hotmail.com>
2016-11-03 14:48:38 -0600

    Update ajedrez-prototipo.js

--- rv/ajedrez-prototipo.js
+++ rv/ajedrez-prototipo.js
@@ -274,9 +274,9 @@ PROTOTIPO.setup = function() {
   PROTOTIPO.escena = new THREE.Scene();
   PROTOTIPO.escena.add( luzP1 );
   PROTOTIPO.camara = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-  PROTOTIPO.camara.position.z = 100;
-  PROTOTIPO.camara.position.y = -100;
-  PROTOTIPO.camara.lookAt( new THREE.Vector3(0,100,0) );
+  PROTOTIPO.camara.position.z = 70;
+  PROTOTIPO.camara.position.y = -70;
+  PROTOTIPO.camara.lookAt( new THREE.Vector3(0,70,0) );
   
   var lienzo = document.getElementById( "ajedrez-prototipo" );
   PROTOTIPO.renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
