04a385fd3ffca7282f2eb092415bdfb7f99e9c5a
cavc94 <chrystian_tezca@hotmail.com>
2016-11-10 15:11:47 -0600

    Update ejemplo-agente.js

--- rv/ejemplo-agente.js
+++ rv/ejemplo-agente.js
@@ -49,9 +49,9 @@ function Pelota( r, x = 0, y = 0 ){
 Pelota.prototype = new Agent();
 Pelota.prototype.sense = function( environment ){
   this.sensor.set( this.position, new THREE.Vector3( 1, 0, 0 ) );
-  var obstaculo1 = this.sensor.instersectObjects( environment.children, true);
+  var obstaculo1 = this.sensor.intersectObjects( environment.children, true);
   this.sensor.set( this.position, new THREE.Vector3( -1, 0, 0 ) );
-  var obstaculo2 = this.sensor.instersectObjects( environment.children, true);
+  var obstaculo2 = this.sensor.intersectObjects( environment.children, true);
   //if ( (obstaculo1.length<0 && (obstaculo1[0].distance<=this.radius) ) || (obstaculo2.length<0 && (obstaculo2[0].distance<=this.radius)) )
   if ( (obstaculo1.length>0 && (obstaculo1.distance<=this.radius) ) || (obstaculo2.length>0 && (obstaculo2.distance<=this.radius)) )
     this.colision = 1;
