3b03faa29e6209bde6ea986754b3b87eb91e2ebf
cavc94 <chrystian_tezca@hotmail.com>
2016-11-22 21:32:00 -0600

    Update tableroc1.js

--- rv/tableroc1.js
+++ rv/tableroc1.js
@@ -291,7 +291,7 @@ Caballo.prototype.sense=function(environment)
 Caballo.prototype.plan=function(environment)
 {
   this.actuator.commands=[]; 
-  if( ((Math.abs(x-X)<=20 && Math.abs(y-Y)<=10) || (Math.abs(x-X)<=10 && Math.abs(y-Y)<=20)) && x!==X && y!==Y && Math.abs(x-X)!=Math.abs(y-Y) ){
+  if( ((Math.abs(x-X)<=20 && Math.abs(y-Y)<=10) || (Math.abs(x-X)<=10 && Math.abs(y-Y)<=20)) && Math.abs(x-X)!=Math.abs(y-Y) ){
     if(X!==x)
       this.actuator.commands.push('goStraightX');
     else if(X===x&&Y!==y) 
