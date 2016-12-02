eb996124082130b67bc0dc4dc6e9029991a0e2ec
cavc94 <chrystian_tezca@hotmail.com>
2016-12-02 14:23:01 -0600

    Update colision.js

--- rv/colision.js
+++ rv/colision.js
@@ -418,20 +418,18 @@ Alfil.prototype.sense=function(environment){
   }
   var obstaculo=this.sensor.intersectObjects(environment.children,true);    
   if( obstaculo.length>0 && obstaculo[0].object.parent.sTP !== this.sTP ){
-    /*if( obstaculo[0].distance<=10*Math.sqrt(2) )
-      this.sensor.colision=false;*/
-    if ( obstaculo[0].distance<10*Math.sqrt(2) )
+    /*if ( obstaculo[0].distance<10*Math.sqrt(2) )
       this.sensor.colision=false;
     else
-      this.sensor.colision=true;
-    /*if ( obstaculo[0].distance>10*Math.sqrt(2) && obstaculo[0].distance<Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2)) )
+      this.sensor.colision=true;*/
+    if ( obstaculo[0].distance>10*Math.sqrt(2) && obstaculo[0].distance<Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2)) )
         this.sensor.colision=true;
       else
         this.sensor.colision=false;
-      if (X===obstaculo[0].position.x&&Y===obstaculo[0].position.y)
+      /*if (X===obstaculo[0].position.x&&Y===obstaculo[0].position.y)
         obstaculo[0].object.position.set(60,-50,0);
-    }*/   
-  }
+    }   
+  }*/
   else if ( obstaculo.length>0 && obstaculo[0].object.parent.sTP === this.sTP  ){
     if( obstaculo[0].distance<Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2)) )
       this.sensor.colision=true;  
