326ade5922916a0250634c5329220be58c4ada57
cavc94 <chrystian_tezca@hotmail.com>
2016-12-02 21:22:09 -0600

    Update colision.js

--- rv/colision.js
+++ rv/colision.js
@@ -420,7 +420,7 @@ Alfil.prototype.sense=function(environment){
   if( obstaculo.length>0 && obstaculo[0].object.parent.sTP !== this.sTP ){
     if ( Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2))<=(obstaculo[0].distance+10*Math.sqrt(2)) ){
       this.sensor.colision=false;
-      if (Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2))<=0.5)
+      if (Math.sqrt(Math.pow(X-x,2)+Math.pow(Y-y,2))<=1)
         obstaculo[0].object.position.set(55,-50,0);
     }
     else
