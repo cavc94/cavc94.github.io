466a722b80d5a12a9178b3cfdf9641e253af26fd
cavc94 <chrystian_tezca@hotmail.com>
2016-12-11 23:50:02 -0600

    Update colision.js

--- rv/colision.js
+++ rv/colision.js
@@ -787,7 +787,7 @@ Peon.prototype=new Agent();
 Peon.prototype.sense=function(environment){
   if (this.sTP === true){
     if (y===Y&&x!==X){
-      this.sensor.set(this.position, new THREE.Vector3(0, 1, 0));
+      this.sensor.set(this.position, new THREE.Vector3(1, 0, 0));
       var diagon=false;
     }
     else if (Y!==y&&X!==x&&Math.abs(y-Y)===Math.abs(x-X)){
@@ -800,7 +800,7 @@ Peon.prototype.sense=function(environment){
   }
   else{
     if (y===Y&&x!==X){
-      this.sensor.set(this.position, new THREE.Vector3(0, -1, 0));
+      this.sensor.set(this.position, new THREE.Vector3(-1, 0, 0));
       var diagon=false;
     }
     else if (Y!==y&&X!==x&&Math.abs(y-Y)===Math.abs(x-X)){
