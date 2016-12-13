5e134b049fea0d394f45c2f372c22dd1349dc255
cavc94 <chrystian_tezca@hotmail.com>
2016-12-12 18:27:57 -0600

    Update colision.js

--- rv/colision.js
+++ rv/colision.js
@@ -154,18 +154,6 @@ Agent.prototype.operations.goStraightY=function(pieza,distance)
   pieza.position.y+=distance*Math.cos(pieza.rotation.z);
 };
 
-Agent.prototype.operations.goStraightZ=function(pieza,distance)
-{
-  if(distance===undefined)
-   {
-    if(pieza.position.z<9.5*1.3)
-      distance=0.5;
-    else
-      distance=0; 
-  }
-  pieza.position.z+=distance;
-};
-
 Agent.prototype.operations.goDiagonal=function(pieza,distance)
 {
   if(distance===undefined)
@@ -433,7 +421,8 @@ Caballo.prototype.plan=function(environment)
   this.actuator.commands=[];
   if(this.sensor.colision === false){
     if( ((Math.abs(x-X)<=20 && Math.abs(y-Y)<=10) || (Math.abs(x-X)<=10 && Math.abs(y-Y)<=20)) && Math.abs(x-X)!==Math.abs(y-Y) ){
-      this.actuator.commands.push('goStraightZ');
+      if(this.position.x=X&&this.position.y=Y)
+        this.position.z = 9.5*1.3;
       if(X!==x&&Y!==y){
         this.actuator.commands.push('goStraightX');
         this.cnt = true;
